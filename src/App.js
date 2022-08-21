a073d9a3c80825ebef3b0b5469fadba6142aab1b
Emre BASBAYDAR <EmreMacbook@Emres-MacBook-Pro.local>
2022-08-21 18:56:21 -0400

    chart added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
-import LineChart from './LineChart';
+import "./App.css";
+import Histogram from "./charts/Histogram";
+import LineChart from "./charts/LineChart";
+import LineChart2 from "./charts/LineChart2";
+import TimeSeries from "./charts/TimeSeries";
 
 function App() {
   return (
     <div className="App">
-      <LineChart/>
+      <div className="row">
+        <LineChart2 height={400} width={400} />
+        <TimeSeries height={400} width={400} />
+        <Histogram  height={550} width={700} />
+      </div>
+
     </div>
   );
 }
